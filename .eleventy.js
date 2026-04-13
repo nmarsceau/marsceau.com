71adada2d848253628608e2dc4033d153822ad8d
Nick Marsceau <nickmarsceau@gmail.com>
2026-04-13 10:17:36 -0400

    Split postroll into multiple files by year

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const slugify = require('slugify');
+const fs = require('fs');
+const path = require('path');
 const eleventyNavigation = require('@11ty/eleventy-navigation');
 const eleventySyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const feedPlugin = require('./src/_config/feedPlugin.js');
@@ -24,6 +26,16 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter('filterOut', (list, remove) => list.filter(item => !remove.includes(item)));
 	eleventyConfig.addFilter('toLocaleDateString', (date) => (new Date(date)).toLocaleDateString());
 	eleventyConfig.addFilter('removeDeadBookmarks', list => list.filter(item => !item.dead));
+
+	eleventyConfig.addGlobalData('postrollItems', () => {
+		const postrollDirectory = path.join(__dirname, 'src/_data/postroll');
+		const postrollFiles = fs.readdirSync(postrollDirectory)
+			.filter(file => /^\d{4}\.json$/.test(file))
+			.sort();
+
+		return ['unknown.json', ...postrollFiles]
+			.flatMap(file => JSON.parse(fs.readFileSync(path.join(postrollDirectory, file), 'utf8')));
+	});
 
 	eleventyConfig.addBundle("css");
 	eleventyConfig.addBundle("js");
